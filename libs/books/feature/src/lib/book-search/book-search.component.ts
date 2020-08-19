b6d34f33178f33a546df607128de5cc1cf1a771f
rosa0219 <rpanda0219@gmail.com>
2020-08-19 17:37:24 -0400

    Instant Search added

---
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit , DoCheck } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  addToReadingList,
+  clearSearch,
+  getAllBooks,
+  ReadingListBook,
+  searchBooks
+} from '@tmo/books/data-access';
+import { FormBuilder } from '@angular/forms';
+import { Book } from '@tmo/shared/models';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'tmo-book-search',
+  templateUrl: './book-search.component.html',
+  styleUrls: ['./book-search.component.scss']
+})
+export class BookSearchComponent implements OnInit {
+  books: ReadingListBook[];
+
+  searchForm = this.fb.group({
+    term: ''
+  });
+
+  constructor(
+    private readonly store: Store,
+    private readonly fb: FormBuilder,
+    private _snackBar: MatSnackBar
+  ) {}
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+  get searchTerm(): string {
+    return this.searchForm.value.term;
+  }
+
+  ngOnInit(): void {
+    this.store.select(getAllBooks).subscribe(books => {
+      this.books = books;
+    });
+    
+  }
+  ngDoCheck(){
+    this.searchBooks()
+  }
+  formatDate(date: void | string) {
+    return date
+      ? new Intl.DateTimeFormat('en-US').format(new Date(date))
+      : undefined;
+  }
+
+  addBookToReadingList(book: Book) {
+    this.store.dispatch(addToReadingList({ book }));
+    this.openSnackBar(book.title+" book","added successfully")
+  }
+
+  searchExample() {
+    this.searchForm.controls.term.setValue('javascript');
+    this.searchBooks();
+  }
+
+  searchBooks() {
+    if (this.searchForm.value.term) {
+      this.store.dispatch(searchBooks({ term: this.searchTerm }));
+    } else {
+      this.store.dispatch(clearSearch());
+    }
+  }
+
+}
